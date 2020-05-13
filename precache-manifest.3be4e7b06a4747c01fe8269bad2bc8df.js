ea16a3c9574e7358a76f7d0c223cfb83ce76d54f
“vishnu <vishnu@bflgroup.ae>
2020-05-13 09:33:44 +0400

    Updates

---
+++ precache-manifest.3be4e7b06a4747c01fe8269bad2bc8df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e4fb64d892a89f03e2f02a22efa1d2",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "7809528ffc4830287121",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1bc7dcb0ddda7c2efa0",
+    "url": "/React-Deploy/static/js/2.d6d43cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Deploy/static/js/2.d6d43cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7809528ffc4830287121",
+    "url": "/React-Deploy/static/js/main.dd2176b6.chunk.js"
+  },
+  {
+    "revision": "164aa664f9f356b8f95c",
+    "url": "/React-Deploy/static/js/runtime-main.39491b0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
